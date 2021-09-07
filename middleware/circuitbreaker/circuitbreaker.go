f3de3cc8b021fbad17f29df3c53f7a37dab1874e
Kagaya <kagaya85@outlook.com>
2021-09-07 20:29:08 +0800

    init group

--- middleware/circuitbreaker/circuitbreaker.go
+++ middleware/circuitbreaker/circuitbreaker.go
@@ -12,41 +12,50 @@ import (
 // Option is circuit breaker option.
 type Option func(*options)
 
-// WithBreaker set circuit breaker implentation
-func WithBreaker(breaker circuitbreaker.CircuitBreaker) Option {
+// WithNewBeaker set the New function of circuit breaker implentation
+func WithNewFunc(newFunc circuitbreaker.NewFunc) Option {
 	return func(o *options) {
-		o.breaker = breaker
+		o.newFunc = newFunc
+	}
+}
+
+// WithName set the breaker name for target
+func WithName(name string) Option {
+	return func(o *options) {
+		o.name = name
 	}
 }
 
 type options struct {
-	breaker circuitbreaker.CircuitBreaker
+	name          string
+	newFunc       circuitbreaker.NewFunc
+	fallbackFuncs []circuitbreaker.FallbackFunc
 }
 
 // Client circuitbreaker middleware will return errBreakerTriggered when the circuit
 // breaker is triggered and the request is rejected directly.
 func Client(opts ...Option) middleware.Middleware {
 	options := &options{
-		breaker: sre.NewBreaker(),
+		newFunc: func() circuitbreaker.CircuitBreaker { return sre.NewBreaker() },
 	}
+
 	for _, o := range opts {
 		o(options)
 	}
+
+	// create breaker group
+	group := &circuitbreaker.Group{
+		New: options.newFunc,
+	}
+
 	return func(handler middleware.Handler) middleware.Handler {
-		return func(ctx context.Context, req interface{}) (interface{}, error) {
-			if err := options.breaker.Allow(); err != nil {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			if gerr := group.Do(options.name, func() error {
+				reply, err = handler(ctx, req)
+				return err
+			}, options.fallbackFuncs...); gerr != nil {
 				// rejected
-				// NOTE: when client reject requets locally,
-				// continue add counter let the drop ratio higher.
-				options.breaker.MarkFailed()
-				return nil, errors.New(503, "CIRCUITBREAKER", "request failed due to circuit breaker triggered")
-			}
-			// allowed
-			reply, err := handler(ctx, req)
-			if err != nil {
-				options.breaker.MarkFailed()
-			} else {
-				options.breaker.MarkSuccess()
+				return reply, errors.New(503, "CIRCUITBREAKER", "request failed due to circuit breaker triggered")
 			}
 			return reply, err
 		}
